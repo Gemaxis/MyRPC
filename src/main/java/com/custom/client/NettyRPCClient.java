e8525f516655b3b5d504cbc4695e3aaf1b0fc089
gemaxis <gemaxis@stumail.nwu.edu.cn>
2025-06-25 16:51:31 +0800

    V13: Style fix, 修复项目中魔法值。

--- src/main/java/com/custom/client/NettyRPCClient.java
+++ src/main/java/com/custom/client/NettyRPCClient.java
@@ -68,7 +68,7 @@ public class NettyRPCClient implements RPCClient {
             // 阻塞的获得结果，通过给channel设计别名，获取特定名字下的channel中的内容（这个在hanlder中设置）
             // AttributeKey是，线程隔离的，不会由线程安全问题。
             // 实际上不应通过阻塞，可通过回调函数
-            AttributeKey<RPCResponse> key = AttributeKey.valueOf("RPCResponse");
+            AttributeKey<RPCResponse> key = AttributeKey.valueOf(RPCResponse.ATTR_KEY);
             RPCResponse response = channel.attr(key).get();
 
             System.out.println(response);
