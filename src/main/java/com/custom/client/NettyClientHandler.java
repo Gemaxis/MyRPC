790fb9abf98ee133db06fb94ad16be0b46f1eef5
gemaxis <gemaxis@stumail.nwu.edu.cn>
2025-06-25 16:51:31 +0800

    V13: Style fix, 修复项目中魔法值。

--- src/main/java/com/custom/client/NettyClientHandler.java
+++ src/main/java/com/custom/client/NettyClientHandler.java
@@ -15,7 +15,7 @@ public class NettyClientHandler extends SimpleChannelInboundHandler<RPCResponse>
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RPCResponse msg) throws Exception {
         // 接收到response, 给channel设计别名，让sendRequest里读取response
-        AttributeKey<RPCResponse> key = AttributeKey.valueOf("RPCResponse");
+        AttributeKey<RPCResponse> key = AttributeKey.valueOf(RPCResponse.ATTR_KEY);
         ctx.channel().attr(key).set(msg);
         ctx.channel().close();
     }
